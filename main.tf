42359657a51449437545e2388a9cd82805d7c14c
vamshidevops206 <164602745+vamshidevops206@users.noreply.github.com>
2024-06-21 21:49:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "web_server_as" {
     image_id           = "ami-0eaf7c3456e7b5b68"
     instance_type = "t2.micro"
-    key_name = "docker-pipeline"
+    key_name = "terraform-key-pair"
 }
    
 
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-01801d057d263ad13", "subnet-0bcc1a1ec83ddb757"]
+     subnets = ["subnet-0e1118fe2a17d4558", "subnet-0a2b5ca45a52a323c"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
